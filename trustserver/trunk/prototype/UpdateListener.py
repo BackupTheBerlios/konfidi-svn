3bdfdcc3dc8e819b8dc6c00bbc057766a3ee8b38
brondsem <brondsem@565a5b3b-b8e8-0310-912b-abc8725082d1>
2005-02-12 16:30:17 +0000

    use OWL from SVN

--- trustserver/trunk/prototype/UpdateListener.py
+++ trustserver/trunk/prototype/UpdateListener.py
@@ -13,12 +13,12 @@ class UpdateListener(SocketServer.BaseRequestHandler):
 	def setup(self):
 		# Create a namespace object for the Friend of a friend namespace.
 		self.FOAF = Namespace("http://xmlns.com/foaf/0.1/")
-		self.TRUST = Namespace("http://www.abundantplunder.com/trust/owl/trust.owl#")
+		self.TRUST = Namespace("http://brondsema.gotdns.com/svn/dmail/foafserver/trunk/schema/trust.owl")
 		self.WOT = Namespace("http://xmlns.com/wot/0.1/")
 		self.RDF = Namespace("http://www.w3.org/2000/01/rdf-schema#")
 		# load trust values into list for later
 		trust = TripleStore()
-		trust.load("http://www.abundantplunder.com/trust/owl/trust.owl#")
+		trust.load("http://brondsema.gotdns.com/svn/dmail/foafserver/trunk/schema/trust.owl")
 		self.trustValues = []
 		for s in trust.subjects(self.RDF["subPropertyOf"], self.TRUST["trustValue"]):
 			self.trustValues.append(s)
