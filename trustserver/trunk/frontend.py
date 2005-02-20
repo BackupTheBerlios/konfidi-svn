4ad0c2d979019c9dfdd8cb2810cbc56f296fe42a
ams5 <ams5@565a5b3b-b8e8-0310-912b-abc8725082d1>
2005-02-20 15:30:23 +0000

    fix bloody whitespace

--- trustserver/trunk/frontend.py
+++ trustserver/trunk/frontend.py
@@ -17,47 +17,47 @@ frontend_version = "0.1"
 
 # our own error class
 class FrontendError(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
 
 #
 # Simple utility functions
 #
 
 def uniqueURI(req):
-    """Returns the URI portion unique to this request, disregarding the domain, real directory, etc"""
-    req.add_common_vars()
-    uri = req.filename[len(os.path.dirname(__file__))+1:]
-    try:
-        uri += req.subprocess_env['PATH_INFO']
-    except KeyError:
-        uri += ""
-    return uri
+	"""Returns the URI portion unique to this request, disregarding the domain, real directory, etc"""
+	req.add_common_vars()
+	uri = req.filename[len(os.path.dirname(__file__))+1:]
+	try:
+		uri += req.subprocess_env['PATH_INFO']
+	except KeyError:
+		uri += ""
+	return uri
 
 def ishex(string):
-    return re.search('^[A-F0-9]+$', string)
+	return re.search('^[A-F0-9]+$', string)
 
 
 def handler(req):
-    """main handler; called by mod_python"""
-    req.allow_methods(["GET"])
+	"""main handler; called by mod_python"""
+	req.allow_methods(["GET"])
     
-    if req.get_config().has_key('PythonDebug'):
-        reload(dump)
+	if req.get_config().has_key('PythonDebug'):
+		reload(dump)
     
-    if (uniqueURI(req) == "test"):
-    	return test(req)
-    if (uniqueURI(req) == "query"):
-        return query(req)
-    if (uniqueURI(req) == "form"):
-        return form(req)
-    if (uniqueURI(req) == ""):
-        return index(req)
+	if (uniqueURI(req) == "test"):
+		return test(req)
+	if (uniqueURI(req) == "query"):
+		return query(req)
+	if (uniqueURI(req) == "form"):
+		return form(req)
+	if (uniqueURI(req) == ""):
+		return index(req)
     #elif (req.method == "GET"):
     #    return get(req)
-    else:
+	else:
 		return apache.HTTP_NOT_IMPLEMENTED
 
 #
@@ -67,19 +67,19 @@ def handler(req):
 def index(req):
 	req.content_type = "text/html"
 	req.write("""
-    <html>
-    <head><title>TrustServer Frontend</title></head>
-    <body>
-    <h2>TrustServer Frontend v""" + frontend_version + """</h2>
+	<html>
+	<head><title>TrustServer Frontend</title></head>
+	<body>
+	<h2>TrustServer Frontend v""" + frontend_version + """</h2>
+
+	<h4>Querying</h4>
+	Just do a GET using to the url "query", defining the variables "source", "sink", and "subject"
+	<br>
+	<a href="query?source=Schamp&sink=Goforth&subject=default">sample: Schamp, Goforth, default</a><br>
+	<h4>Web interface</h4>
+	Or use <a href="form">this form</a><br>
     
-    <h4>Querying</h4>
-    Just do a GET using to the url "query", defining the variables "source", "sink", and "subject"
-    <br>
-    <a href="query?source=Schamp&sink=Goforth&subject=default">sample: Schamp, Goforth, default</a><br>
-    <h4>Web interface</h4>
-    Or use <a href="form">this form</a><br>
-    
-    <h4><a href="test">debug output</a></h4>
+	<h4><a href="test">debug output</a></h4>
 	</body>
 	</html>
 	""")
@@ -119,77 +119,77 @@ def query(req):
 	return apache.OK    
 
 def form(req):
-    req.content_type = "text/html"
-    req.write("""
-    <html>
-    <head><title>TrustServer Query form</title></head>
-    <body>
-    <a href="./">Back to index</a>
-    """)
+	req.content_type = "text/html"
+	req.write("""
+	<html>
+	<head><title>TrustServer Query form</title></head>
+	<body>
+	<a href="./">Back to index</a>
+	""")
     
-    req.write("""
-    Enter your query: <br>
-    <form action="query" method="POST">
-    Source: <input type="text" name="source"><br/>
-    Sink: <input type="text" name="sink"><br/>
-    Subject: <input type="text" name="subject"><br/>
-    <br/>
-    <input type="submit" name="submit" value="Submit">
-    </form>
-    </body></html>""")
-    return apache.OK
+	req.write("""
+	Enter your query: <br>
+	<form action="query" method="POST">
+	Source: <input type="text" name="source"><br/>
+	Sink: <input type="text" name="sink"><br/>
+	Subject: <input type="text" name="subject"><br/>
+	<br/>
+	<input type="submit" name="submit" value="Submit">
+	</form>
+	</body></html>""")
+	return apache.OK
 
 #
 # test stuff; delete sometime
 #
 
 def test(req):
-    req.content_type = "text/plain"
+	req.content_type = "text/plain"
+
+	apache.log_error("request!", apache.APLOG_NOTICE)
+	req.write("\nParsed URI:\n-------------\n")
+	req.write(dump.dump(req.parsed_uri))
+	req.write("\nModPython Options:\n-------------\n")
+	req.write(dump.dump(req.get_options()))
+	req.write("\nModPython Config:\n-------------\n")
+	req.write(dump.dump(req.get_config()))
     
-    apache.log_error("request!", apache.APLOG_NOTICE)
-    req.write("\nParsed URI:\n-------------\n")
-    req.write(dump.dump(req.parsed_uri))
-    req.write("\nModPython Options:\n-------------\n")
-    req.write(dump.dump(req.get_options()))
-    req.write("\nModPython Config:\n-------------\n")
-    req.write(dump.dump(req.get_config()))
+	req.write("\nOS Env:\n-------------\n")
+	req.write(dump.dump(os.environ))
+
+	req.write("\nProcess Env:\n-------------\n")
+	req.add_common_vars()
+	req.write(dump.dump(req.subprocess_env))
     
-    req.write("\nOS Env:\n-------------\n")
-    req.write(dump.dump(os.environ))
+	req.write("\n")
+	req.write("server_root=" + apache.server_root() + "\n")
+	req.write("document_root=" + req.document_root() + "\n")
+	req.write("loglevel=" + dump.dump(req.server.loglevel))
+	req.write("is_virtual=" + dump.dump(req.server.is_virtual))
+	req.write("phase=" + dump.dump(req.phase))
+	req.write("handler=" + dump.dump(req.handler))
+	req.write("uri=" + dump.dump(req.uri))
+	req.write("filename=" + dump.dump(req.filename))
+	req.write("py interpreter=" + dump.dump(req.interpreter))
     
-    req.write("\nProcess Env:\n-------------\n")
-    req.add_common_vars()
-    req.write(dump.dump(req.subprocess_env))
+	req.write("\n")
+	req.write("__file__=" + __file__ + "\n")
+	req.write("dir=" + os.path.dirname(__file__) + "\n")
     
-    req.write("\n")
-    req.write("server_root=" + apache.server_root() + "\n")
-    req.write("document_root=" + req.document_root() + "\n")
-    req.write("loglevel=" + dump.dump(req.server.loglevel))
-    req.write("is_virtual=" + dump.dump(req.server.is_virtual))
-    req.write("phase=" + dump.dump(req.phase))
-    req.write("handler=" + dump.dump(req.handler))
-    req.write("uri=" + dump.dump(req.uri))
-    req.write("filename=" + dump.dump(req.filename))
-    req.write("py interpreter=" + dump.dump(req.interpreter))
-    
-    req.write("\n")
-    req.write("__file__=" + __file__ + "\n")
-    req.write("dir=" + os.path.dirname(__file__) + "\n")
-    
-    req.write("\n")
-    if apache.mpm_query(apache.AP_MPMQ_IS_THREADED):
-        req.write("mpm is threaded\n")
-    else:
-        req.write("mpm is NOT threaded\n")
-    if apache.mpm_query(apache.AP_MPMQ_IS_FORKED):
-        req.write("mpm is forked\n")
-    else:
-        req.write("mpm is NOT forked\n")
+	req.write("\n")
+	if apache.mpm_query(apache.AP_MPMQ_IS_THREADED):
+		req.write("mpm is threaded\n")
+	else:
+		req.write("mpm is NOT threaded\n")
+	if apache.mpm_query(apache.AP_MPMQ_IS_FORKED):
+		req.write("mpm is forked\n")
+	else:
+		req.write("mpm is NOT forked\n")
         
-    req.write("\n")
-    req.write("POST form data:\n")    
-    req.write("content length: " + dump.dump(req.clength))
-    req.write(dump.dump(req.read()))
-    #req.write(dump.dump(apache.config_tree()))
+	req.write("\n")
+	req.write("POST form data:\n")    
+	req.write("content length: " + dump.dump(req.clength))
+	req.write(dump.dump(req.read()))
+	#req.write(dump.dump(apache.config_tree()))
     
-    return apache.OK
+	return apache.OK
