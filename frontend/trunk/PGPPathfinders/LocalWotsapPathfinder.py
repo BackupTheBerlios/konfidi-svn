551d90fa4bd4da0eda023b97027d49d4c0978c66
ams5 <ams5@565a5b3b-b8e8-0310-912b-abc8725082d1>
2005-03-03 04:39:27 +0000

    did local instantiation of wotsap instead of shell.  stdout, stderr wrappers

--- frontend/trunk/PGPPathfinders/LocalWotsapPathfinder.py
+++ frontend/trunk/PGPPathfinders/LocalWotsapPathfinder.py
@@ -1,21 +1,32 @@
 from PGPPathfinder import PGPPathfinder
 import os
+from StringIO import StringIO
 
 class LocalWotsapPathfinder(PGPPathfinder):
-    """Uses wotsap from http://www.lysator.liu.se/~jc/wotsap/ and it's nightly-generated dump file"""
-    
-    def graph(self, source, sink, limit=None):
-        (out, err) = self.runwotsap(source, sink, limit)
-        return err + out
+	"""Uses wotsap from http://www.lysator.liu.se/~jc/wotsap/ and it's nightly-generated dump file"""
+	def graph(self, source, sink, limit=None):
+		(out, err) = self.runwotsap(source, sink, limit)
+		return err + out
 
-    def connected(self, source, sink, limit=None):
-        (out, err) = self.runwotsap(source, sink, limit)
-        return len(out) != 0 and len(err) == 0
+	def connected(self, source, sink, limit=None):
+		(out, err) = self.runwotsap(source, sink, limit)
+		return len(out) != 0 and len(err) == 0
 
-    def runwotsap(self, source, sink, limit=None):
-        """note how wotsap doesn't support a long fingerprint, so we just take the last 8 chars"""
-        (stdin, stdout, stderr) = os.popen3("python %s -w %s %s %s" % (self.config["app"], self.config["data"], source[-8:], sink[-8:]))
-        stdin.close()
-        out = stdout.read()
-        err = stderr.read()
-        return (out, err)
+	def runwotsap(self, source, sink, limit=None):
+		"""note how wotsap doesn't support a long fingerprint, so we just take the last 8 chars"""
+		exec("from %s import wotsapmain" % (self.config["app"])
+		out = StringIO()
+		err = StringIO()
+		oldout = sys.stdout
+		olderr = sys.stderr
+		
+		sys.stdout = out
+		sys.stdout = err
+		
+		wotsapmain(["-w", self.config["data"], source[-8:], sink[-8:])
+		
+		out = out.getvalue()
+		err = err.getvalue()
+		sys.stdout = oldout
+		sys.stderr = olderr
+		return (out, err)
