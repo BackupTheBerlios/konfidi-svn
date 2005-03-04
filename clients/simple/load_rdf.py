1c72c8fe1ecd6fd570f0dfe332713555919de303
ams5 <ams5@565a5b3b-b8e8-0310-912b-abc8725082d1>
2005-03-04 16:01:10 +0000

    movign stuff around

--- clients/simple/load_rdf.py
+++ clients/simple/load_rdf.py
@@ -2,7 +2,7 @@ import sys
 import os
 import re
 
-tc = "/usr/bin/python /home/ams5/public_html/trustserver/TrustClient.py"
+tc = "svn cat http://brondsema.gotdns.com/svn/dmail/clients/simple/TrustClient.py"
 files = [os.path.abspath(f) for f in os.listdir('.') if os.path.isfile(os.path.join('.', f)) and re.compile(".rdf$").search(f, 1)]
 for f in files:
-	os.system("%s localhost 20010 %s" % (tc, f))
+	os.system("%s localhost 20010 %s | /usr/bin/python" % (tc, f))
