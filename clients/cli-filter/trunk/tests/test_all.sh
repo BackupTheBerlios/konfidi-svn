24a27990d352eabfb4b7b90b500ea3d4e82d4f4c
brondsem <brondsem@565a5b3b-b8e8-0310-912b-abc8725082d1>
2005-03-17 01:09:25 +0000

    bit better

--- clients/cli-filter/trunk/tests/test_all.sh
+++ clients/cli-filter/trunk/tests/test_all.sh
@@ -2,6 +2,8 @@
 
 # any arguments (e.g. -v) get passed on to APP
 
+# *.headers must be sorted (use 'sort' command)
+
 APP=../dmail-cli-filter
 
 function namename()
@@ -11,8 +13,8 @@ function namename()
   echo "${name0:-$name}"
 }
 
-ls already_has_headers.txt | while read testcase
+ls plain.txt | while read testcase
 do
-	$APP $* < $testcase | comm $(namename $testcase).headers -
+	$APP $* < $testcase 2>/dev/null | sort | comm -1 -2 sort $(namename $testcase).headers -
 	
 done
