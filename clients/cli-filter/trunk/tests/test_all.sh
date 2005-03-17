a83f1a396664c7aadb2309ff06b9d30a8be45ec5
brondsem <brondsem@565a5b3b-b8e8-0310-912b-abc8725082d1>
2005-03-17 05:02:25 +0000

    document

--- clients/cli-filter/trunk/tests/test_all.sh
+++ clients/cli-filter/trunk/tests/test_all.sh
@@ -20,11 +20,16 @@ do
 	echo $testname:
 	if [ -f $checkfile ]
 	then
+		# run app (ignore stderr)
+		# sort output
+		# get lines which are common to it and checkfile
+		# bytewise comparison of that with the checkfile (ignore stderr)
+		# if-stmt on result of that bytewise comparison
 		if $APP $* < $testcase 2>/dev/null | sort | comm -1 -2 $checkfile - | cmp $checkfile - 2>/dev/null
 		then
-		  echo "    PASS"
+			echo "    PASS"
 		else
-		  echo "    FAIL"
+			echo "    FAIL"
 		fi
 	else
 		echo "    no check file ($checkfile)"
