63f7524f188d3a21df3c95724efed3799f422139
brondsem <brondsem@565a5b3b-b8e8-0310-912b-abc8725082d1>
2005-04-18 17:31:11 +0000

    update with new repo, and all include all dirs

--- co_trunks.sh
+++ co_trunks.sh
@@ -3,27 +3,29 @@
 #
 # To run this (without even checking it out!) execute:
 #
-# mkdir dmail; cd dmail # this is an umbrella dir for all the dmail apps
-# svn cat http://brondsema.gotdns.com/svn/dmail/co_trunks.sh | bash
+# mkdir konfidi; cd konfidi # this is an umbrella dir for all the konfidi apps
+# svn cat svn+ssh://svn.berlios.de/svnroot/repos/konfidi/co_trunks.sh | bash
 #
 
 # You can override BASE and TBT like this:
-# svn cat http://brondsema.gotdns.com/svn/dmail/co_trunks.sh | TBT=tags/1.0-beta bash
+# svn cat svn+ssh://svn.berlios.de/svnroot/repos/konfidi/co_trunks.sh | TBT=tags/1.0-beta bash
 
 if [ ! "$BASE" ]
 then
-	BASE=https://brondsema.gotdns.com/svn/dmail
+        BASE=svn+ssh://svn.berlios.de/svnroot/repos/konfidi
 fi
 if [ ! "$TBT" ]
 then
-	TBT=trunk
+        TBT=trunk
 fi
 
 ####
 ####
 
-svn co $BASE/tests
-for app in clients/cli-filter foafserver frontend tests trustserver
+svn co $BASE/paper
+svn co $BASE/presentation
+svn co $BASE/clients/simple clients/simple
+for app in clients/cli-filter foafserver frontend tests trustserver schema
 do
-	svn co $BASE/$app/$TBT $app
+        svn co $BASE/$app/$TBT $app
 done
