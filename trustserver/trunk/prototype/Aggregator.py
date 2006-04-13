74e174a571ea9e14af656e5cbd891b7e8fac4019
brondsem <brondsem@565a5b3b-b8e8-0310-912b-abc8725082d1>
2006-04-13 01:59:01 +0000

    delete unused files

--- trustserver/trunk/prototype/Aggregator.py
+++
@@ -1,4 +0,0 @@
-class Aggregator(object):
-	def __init__(self):
-		pass
-	
--- trustserver/trunk/prototype/sample_loader.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-
-files = ["laing.rdf", "brondsema.rdf", "schamp.rdf"]
-for f in files:
-	os.system("TrustClient.py localhost 50010 %s" % f)
